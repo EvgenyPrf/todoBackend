f60274d1f41e414c5a643df4c044fa8cfce95527
Evgeniy Parfenyuk <info.jacktiger@gmail.com>
2024-07-21 21:06:47 +0300

    add new methods (add_entry, print entries)

--- resources.py
+++ resources.py
@@ -4,6 +4,15 @@ class Entry:
             entries = []
         self.title = title
         self.parent = parent
+        self.entries = entries
 
     def __str__(self):
         return self.title
+
+    def add_entry(self, entry):
+        self.entries.append(entry)
+        entry.parent = self
+
+    def print_entries(self):
+        for e in self.entries:
+            print(e)
