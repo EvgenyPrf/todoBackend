e8162a757b7c54c43d3be722018b7b3438216a20
evgenyparfenyuk <evgenii.parfeniuk@onlinesup.com>
2024-07-21 17:52:41 +0300

    add Entry

---
+++ resources.py
@@ -0,0 +1,9 @@
+class Entry:
+    def __init__(self, title, entries=None, parent=None):
+        if entries is None:
+            entries = []
+        self.title = title
+        self.parent = parent
+
+    def __str__(self):
+        return self.title
