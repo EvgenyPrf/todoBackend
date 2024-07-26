70013d5cb5bd1993fd767d02164fb84875968822
evgenyparfenyuk <evgenii.parfeniuk@onlinesup.com>
2024-07-26 17:06:51 +0300

    add write_json_to_file and read_json_from_file

--- filemanager.py
+++ filemanager.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 def write_to_file(filename, content):
@@ -14,3 +15,21 @@ def read_from_file(filename):
 def print_directory_contents(directory):
     for folder in os.listdir(directory):
         print(os.path.join(directory, folder))
+
+
+def json_dumps(value):
+    return json.dumps(value, indent=4)
+
+
+def json_loads(value):
+    return json.loads(value)
+
+
+def write_json_to_file(filename: str, content: dict):
+    with open(filename, 'w') as f:
+        json.dump(content, f)
+
+
+def read_json_from_file(filename: str):
+    with open(filename, 'r') as f:
+        return json.load(f)
